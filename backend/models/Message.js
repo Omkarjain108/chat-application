bfdcf1c97f81cbf5e90cc3f4f98c2f929741a488
Omkarjain108 <osavaganve@gmail.com>
2025-09-22 21:39:31 +0530

    setup schema for message

---
+++ backend/models/Message.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    role: {
+      type: String,
+      enum: ["user", "assistant"],
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
